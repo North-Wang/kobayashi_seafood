3049d57011d8c9a5f788999a463530223362f5d5
North-Wang <b10234033.ntust@gmail.com>
2022-07-18 08:47:53 +0800

    0718  0847

---
+++ js/merch.js
@@ -0,0 +1,32 @@
+/*滑鼠移動到各種類的特效*/
+var m1 = document.querySelector(".m1");
+var t1 = document.querySelector(".title1");
+
+m1.addEventListener("mouseover",function(){
+    t1.style.color = "#5FBBFF";
+    t1.style.transform = "scale(1.2,1.2)";
+})
+
+var m2 = document.querySelector(".m2");
+var t2 = document.querySelector(".title2");
+
+m2.addEventListener("mouseover",function(){
+    t2.style.color = "#5FBBFF";
+    t2.style.transform = "scale(1.2,1.2)";
+})
+
+var m3 = document.querySelector(".m3");
+var t3 = document.querySelector(".title3");
+
+m3.addEventListener("mouseover",function(){
+    t3.style.color = "#5FBBFF";
+    t3.style.transform = "scale(1.2,1.2)";
+})
+
+var m4 = document.querySelector(".m4");
+var t4 = document.querySelector(".title4");
+
+m4.addEventListener("mouseover",function(){
+    t4.style.color = "#5FBBFF";
+    t4.style.transform = "scale(1.2,1.2)";
+})
