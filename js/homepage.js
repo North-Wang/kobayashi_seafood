ddb97dbfc885d59b0c8ff41b5022884e502e2246
North-Wang <b10234033.ntust@gmail.com>
2022-07-28 18:30:25 +0800

    0728 18:30

--- js/homepage.js
+++ js/homepage.js
@@ -1,6 +1,24 @@
 /*-------------自動輪播圖-------------*/
 
-setInterval
+const wrap_slide = document.getElementsByClassName("wrap-slide")[0];
+    console.log(wrap_slide);
+    //確認有幾張slide
+const slide = document.getElementsByClassName("slidef");
+    console.log(slide.length);
+const length_slid = slide.length;
+
+
+    //間隔幾秒切換slide
+setInterval(function(){
+     for(i = 0; i < slide.length; i++){
+        let a = 0;
+        wrap_slide.setAttribute("style","margin-left:" + a + "px");
+        a++;
+     };   
+    
+},1000);
+ 
+
 
 /*-------------時間倒數器-------------*/
 
@@ -18,12 +36,12 @@ setInterval(function(){ /* 每隔一秒執行匿名函式 */
         //剩下多少分鐘
     let minutes = Math.floor((time_start - hour*3600) / 60);
     if(minutes<10){
-        hour = "0" + minutes;
+        minutes = "0" + minutes;
     }
         //剩下多少秒
     let second = Math.floor(time_start - (hour*3600 + minutes*60));
     if(second<10){
-        hour = "0" + second;
+        second = "0" + second;
     }           
     document.getElementById("countdown").innerHTML = hour + "：" + minutes + "：" + second;
 },1000);
