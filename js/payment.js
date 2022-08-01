5a32871072b32463d50eef77c14f9d9dab85a375
North-Wang <b10234033.ntust@gmail.com>
2022-08-01 17:05:01 +0800

    0801 17:04

--- js/payment.js
+++ js/payment.js
@@ -1,9 +1,15 @@
-/*單選框 點擊事件*/
-var cli_1 = document.querySelector(".radio1");
-var qrcode = document.querySelector(".qrcode");
-var cli_2 = document.querySelector(".radio2");
-var visa_detail = document.querySelector(".visa-detail");
-var mask0 = document.querySelector(".mask0");
+/*------------選擇購買數量-------------*/
+
+let mines = document.querySelector("fa-minus");
+let plus = document.querySelector("fa-plus");
+let merch_num = document.querySelector(".number"); /* 購買的數量 */ 
+
+/*------------擇一付款方式-------------*/
+let cli_1 = document.querySelector(".radio1");
+let qrcode = document.querySelector(".qrcode");
+let cli_2 = document.querySelector(".radio2");
+let visa_detail = document.querySelector(".visa-detail");
+let mask0 = document.querySelector(".mask0");
 
 cli_1.addEventListener("click",function(){
     qrcode.style.display = "none";
@@ -23,10 +29,9 @@ cli_2.addEventListener("click",function(){
     }
 })
 
-/*付款成功 彈出訊息*/
-var submit = document.querySelector(".submit");
-var messa = document.querySelector(".messa");
-var mask0 = document.querySelector(".mask0");
+/*------------付款成功 彈出訊息-------------*/
+let submit = document.querySelector(".submit");
+let messa = document.querySelector(".messa");
 
 submit.addEventListener("click",function(){
     if (messa.style.display === "none"){
